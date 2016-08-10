068512385e8eb0cef98eaff8895d5e13b0f1ff85
Klarizon Emar Motol <klarizonemar@gmail.com>
2016-08-10 15:39:36 +0800

    Added dismiss button

--- resources/views/dashboard/client/_partials/enter_pin.blade.php
+++ resources/views/dashboard/client/_partials/enter_pin.blade.php
@@ -2,7 +2,10 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h4 class="modal-title huge">
+                <button type="button" class="close" data-dismiss="modal">
+                    <i class="fa fa-times fa-2x" aria-hidden="true"></i>
+                </button>
+                <h4 class="modal-title huge text-center">
                     <strong>Nice tap! Enter your pin code.</strong>
                 </h4>
             </div>
--- resources/views/dashboard/client/_partials/tap_card.blade.php
+++ resources/views/dashboard/client/_partials/tap_card.blade.php
@@ -5,7 +5,7 @@
                 <button type="button" class="close" data-dismiss="modal">
                     <i class="fa fa-times fa-2x" aria-hidden="true"></i>
                 </button>
-                <h4 class="modal-title huge">
+                <h4 class="modal-title huge text-center">
                     <strong>Please tap your toot card!</strong>
                 </h4>
             </div>
