7d40dea17612ae549213ead93c752861750d2aa2
Klarizon Emar Motol <klarizonemar@gmail.com>
2016-09-03 03:51:37 +0800

    added operation day settings

--- resources/views/dashboard/admin/settings/_partials/navbar.blade.php
+++ resources/views/dashboard/admin/settings/_partials/navbar.blade.php
@@ -1,3 +1,3 @@
-<li {!! (Route::is('settings.toot_card') ? 'class="active"' : '') !!}>
+<li {!! ((Route::is('settings.toot_card') || Route::is('settings.operation_day')) ? 'class="active"' : '') !!}>
     <a href="{{ route('settings.toot_card') }}">Settings</a>
 </li>
